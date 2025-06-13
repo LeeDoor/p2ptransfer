0b2522dbcfaff4a77e9144d5d67d66100678896c
leedoor <samoshenkoleonid@yandex.ru>
2025-06-13 15:49:46 +0300

    google_tests_now

--- tests/test_argument_parser.cpp
+++ tests/test_argument_parser.cpp
@@ -1,77 +1,9 @@
-#include <catch2/catch_test_macros.hpp>
+#include <gtest/gtest.h>
 #include "../server/argument_parser.hpp"
-#define IS_COUT
-#include "logger.hpp"
 
-SCENARIO ("ArgumentParser parses the data") {
-    Logger::init();
-    ArgumentData res;
+TEST(ArgumentParserTest, Success) {
+    ArgumentData data;
     ArgumentParser parser;
-    GIVEN ("an argument list as char**") {
-        char** argv;
-        WHEN ("trying to parse empty pointer") {
-            bool success = parser.parse_arguments(0, argv, res);
-            THEN ("should return false") {
-                REQUIRE(success == false);
-            }
-        }
-        WHEN ("Allocating memory for one argument and parsing") {
-            argv = new char*;
-            argv[0] = new char[]{"executable"};
-            bool success = parser.parse_arguments(1, argv, res);
-            THEN ("For any data parsing should fail") {
-                REQUIRE(success == false);
-            }
-            delete[] argv[0];
-            delete[] argv;
-        }
-        WHEN ("Allocating memory to 2 elements") {
-            argv = new char*[2];
-            argv[0] = new char[]{"executable"};
-            argv[1] = new char[20];
-            
-            WHEN ("PORT is a positive number") {
-                strcpy(argv[1], "1984");
-                REQUIRE(parser.parse_arguments(2, argv, res));
-                REQUIRE(res.port == 1984);
-
-                strcpy(argv[1], "65535");
-                REQUIRE(parser.parse_arguments(2, argv, res));
-                REQUIRE(res.port == 65535);
-
-                strcpy(argv[1], "0");
-                REQUIRE(parser.parse_arguments(2, argv, res));
-                REQUIRE(res.port == 0);
-
-                strcpy(argv[1], "-0");
-                REQUIRE(parser.parse_arguments(2, argv, res));
-                REQUIRE(res.port == 0);
-
-                strcpy(argv[1], "1489");
-                REQUIRE(parser.parse_arguments(2, argv, res));
-                REQUIRE(res.port == 1489);
-            }
-            WHEN ("PORT is negative value or a string") {
-                strcpy(argv[1], "-1984");
-                REQUIRE(!parser.parse_arguments(2, argv, res));
-
-                strcpy(argv[1], "-65535");
-                REQUIRE(!parser.parse_arguments(2, argv, res));
-
-                strcpy(argv[1], "abobus");
-                REQUIRE(!parser.parse_arguments(2, argv, res));
-
-                strcpy(argv[1], "one");
-                REQUIRE(!parser.parse_arguments(2, argv, res));
-
-                strcpy(argv[1], "");
-                REQUIRE(!parser.parse_arguments(2, argv, res));
-            }
-
-            delete[] argv[0];
-            delete[] argv[1];
-            delete[] argv;
-        }
-    }
-    Logger::destroy();
+    char** argv;
+    ASSERT_TRUE(
 }
---
+++ tests/test_presenter.cpp
@@ -0,0 +1,5 @@
+#include "test_headers.hpp"
+
+TEST (Presenter, TransferCallbacks) {
+
+}
