1a8c14d2066f55c369f70933f2cae866cf57b4ea
leedoor <samoshenkoleonid@yandex.ru>
2025-06-17 15:21:37 +0300

    directories

--- tests/address_gatherer_mock.hpp
+++
@@ -1,7 +0,0 @@
-#include "server/model_address_gather.hpp"
-#include <gmock/gmock.h>
-
-class AddressGathererMock : public IModelAddressGatherer {
-public:
-    MOCK_METHOD(void, gather_local_address, (), (override));
-};
--- tests/network_manager_mock.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "headers.hpp"
-#include "server/model_network_manager.hpp"
-class NetworkManagerMock : public IModelNetworkManager {
-public:
-    MOCK_METHOD(int, init, (Port port), (override));
-};
