66ceb41e9556e6638409b92d93712c7c490f6205
leedoor <samoshenkoleonid@yandex.ru>
2025-07-15 18:39:01 +0300

    doxygen_init

--- general/socket_manager/socket_manager_builder.hpp
+++ general/socket_manager/socket_manager_builder.hpp
@@ -3,6 +3,15 @@
 
 namespace general {
 
+/*!
+* \brief socket builder for immediate connection
+*
+* Using socket builder prevents class user from accessing
+* socket before connection. Takes over the responsibility
+* to ensure the returned socket connection is established.
+* \throws std::runtime_error if socket opening or connection 
+* establishing failed.
+*/
 class SocketManagerBuilder {
 public:
     virtual net::awaitable<std::shared_ptr<SocketManager>> tcp_listening_at(Port port) = 0;
