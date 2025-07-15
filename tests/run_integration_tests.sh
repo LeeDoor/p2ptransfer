e854e900c02719e699a6c82b5e2d8fe0ba7fae1a
leedoor <samoshenkoleonid@yandex.ru>
2025-07-15 18:09:51 +0300

    no_integration_tests

--- tests/run_integration_tests.sh
+++
@@ -1,6 +0,0 @@
-exec=$1/server/p2plisten
-echo "running integration tests on $1. executable is $exec"
-
-echo "TEST #1"
-echo "Should return immediately when passed port is not a number: "
-echo 'notnumber' | $exec 
