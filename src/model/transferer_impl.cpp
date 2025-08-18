b856297776fb014494c6fc790281295ad95715bc
leedoor <samoshenkoleonid@yandex.ru>
2025-08-18 11:08:13 +0300

    moving_headers_to_mocks

--- src/model/transferer_impl.cpp
+++ src/model/transferer_impl.cpp
@@ -15,6 +15,7 @@ TransfererImpl::TransfererImpl(
 void TransfererImpl::transfer_file(const Address& address, Port port, const Filename& filename) {
     if(!thread_wrapper_->is_running()) {
         thread_wrapper_->execute([&] {
+            // auto socket = socket_builder_->tcp_connecting_to();
             Logger::log() << "Sending file at " << filename << " to " << address << ":" << port << std::endl;
         });
     }
