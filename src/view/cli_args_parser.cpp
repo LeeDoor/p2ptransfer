83bb91f997115c3c4044a345e612a508e1202382
leedoor <samoshenkoleonid@yandex.ru>
2025-08-30 13:03:06 +0300

    adding_program_option

--- src/view/cli_args_parser.cpp
+++ src/view/cli_args_parser.cpp
@@ -1,40 +1,70 @@
 #include "cli_args_parser.hpp"
 #include "logger.hpp"
-#include <boost/program_options.hpp>
 
 namespace p2ptransfer {
 namespace view {
 
 CLIArgsParser::CLIArgsParser(int argc, char** argv) 
-    : argc{argc}, argv{argv} {}
+    : argc{argc}
+    , argv{argv}
+    , desc {
+        "(1) p2ptransfer <address> <port> <filename>\n"
+        "(2) p2ptransfer <-h|--host address> <-p|--port port> <-f|--filename filename>\n"
+        "(3) p2ptransfer --get <port>\n"
+        "(4) p2ptransfer --get <-p|--port port>\n"
+    }
+    , args{} 
+{}
 CLIArgsParser::CLIRunArgs CLIArgsParser::parse_cli_args() {
-    namespace po = boost::program_options;
-    CLIRunArgs args{};
     try {
-        po::options_description desc(
-            "(1) p2ptransfer <address> <port> <filename>\n"
-            "(2) p2ptransfer <-h|--host address> <-p|--port port> <-f|--filename filename>\n"
-            "(3) p2ptransfer get <port>\n"
-            "(4) p2ptransfer get <-p|--port port>\n"
-        );
         desc.add_options()
             ("help", "produce help message")
+            ("host,h", po::value<Address>(&args.address), "Address to connect to. Combined with -p <port>")
+            ("port,p", po::value<Port>(&args.port), "Port to connect to OR port to listen at")
+            ("filename,f", po::value<Filename>(&args.filename), "Directory to a file being sent to remote user")
+            ("get", "Indicates the file receiving action")
         ;
-        po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
         if(vm.count("help")) {
             Logger::log() << desc << std::endl;
             return {.valid = false};
         }
-
+        if(vm.count("get"))
+            args.action = Listen;
+        else args.action = Transfer;
+        if(auto problem = validate(); !problem.empty()) {
+            throw std::runtime_error(problem + " provided.");
+        }
     } catch(const std::exception& ex) {
-        Logger::log() << ex.what() << std::endl;
+        Logger::log() 
+            << ex.what() << std::endl
+            << "To see the correct usage guide, use --help" << std::endl;
         args.valid = false;
     }
     return args;
 }
 
+std::string CLIArgsParser::validate() {
+    switch(args.action) {
+        case Transfer:
+            return validate_transfer();
+        case Listen:
+            return validate_listen();
+    }
+}
+
+std::string CLIArgsParser::validate_transfer() {
+    if(!args.port) 
+        throw std::runtime_error("No --port provided");
+    if(!args.address.empty() || !args.filename.empty()) 
+        throw std::runtime_error("Extra arguments provided");
+}
+std::string CLIArgsParser::validate_listen() {
+    if(!args.address.empty())
+        if(!args.port)
+            if(!args.filename.empty())
+}
 
 }
 }
--- src/view/cli_args_parser.hpp
+++ src/view/cli_args_parser.hpp
@@ -2,16 +2,18 @@
 #include "address.hpp"
 #include "port.hpp"
 #include "request_structures.hpp"
+#include <boost/program_options.hpp>
 
 namespace p2ptransfer {
 namespace view {
 
+namespace po = boost::program_options;
 class CLIArgsParser {
 public:
-    enum Action { None, Listen, Transfer };
+    enum Action { Listen, Transfer };
     struct CLIRunArgs {
         bool valid = true;
-        Action action = None;
+        Action action = Transfer;
         Address address = "";
         Port port = 0;
         Filename filename = "";
@@ -21,8 +23,16 @@ public:
 
     CLIRunArgs parse_cli_args();
 private:
+    /*! Should have args.action set before using. */
+    std::string validate();
+    std::string validate_transfer();
+    std::string validate_listen();
+
     int argc;
     char** argv;
+    po::options_description desc;
+    po::variables_map vm;
+    CLIRunArgs args;
 };
 
 }
