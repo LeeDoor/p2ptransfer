6b340845eb206d6d9c1773bf21c024e8428fb816
leedoor <samoshenkoleonid@yandex.ru>
2025-11-05 09:05:12 +0300

    remove false directory

--- client/argument_data.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "stdlib.hpp"
-#include "port.hpp"
-struct ArgumentData {
-    std::string address;
-    Port port;
-    std::string filename;
-};
--- client/tests/main.cpp
+++
@@ -1,8 +0,0 @@
-int main(int argc, char** argv) {
-    ::testing::InitGoogleMock(&argc, argv);
-    
-    ::testing::FLAGS_gmock_default_mock_behavior = 2;
-    ::testing::FLAGS_gtest_stack_trace_depth = 100;
-    
-    return RUN_ALL_TESTS();
-}
